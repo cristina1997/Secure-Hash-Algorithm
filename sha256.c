73df39b972f0e8eacb622cb48564a78a3640d42a
unknown <cristinaflorinaeu@gmail.com>
2019-03-05 15:34:06 +0000

    Added methods

---
+++ sha256.c
@@ -0,0 +1,16 @@
+// Cristina Nita, 2019
+// Secure Hash Algorithm, 256 bit version (SHA256)
+// https://ws680.nist.gov/publication/get_pdf.cfm?pub_id=910977
+
+#include <stdio.h>
+#include <stdint.h>
+
+void sha256(){
+
+}
+
+int main(int argc, char *argv[]){ 
+
+
+  return 0;
+}
