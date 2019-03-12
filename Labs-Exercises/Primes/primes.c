1cc74bea0912c2439c4993d63fdad02a15b2b861
cristina1997 <cristinaflorinaeu@gmail.com>
2019-03-12 15:03:33 +0000

    Comments Added

--- Labs-Exercises/Primes/primes.c
+++ Labs-Exercises/Primes/primes.c
@@ -1,33 +1,38 @@
+// Author: Cristina Nita 2019 - Practical Exercise 1 from Theory of Algorithms Labs
 #include <stdio.h>
 
 int isPrime(unsigned int testPrime){
-  unsigned int i;
+	unsigned int i;
 
-  for (i = 2; i < testPrime ; i++){
-    if (testPrime % i == 0 ){
-	return 0; // returns 0 when no error
-    }
-  }
+	// prime numbers start at 2, the for loop continues until the number inputed is bigger than i
+	for (i = 2; i < testPrime ; i++){
+		// it checks if the testPrime number is divisible by any number between 2 and testPrime - 1
+		// if not, the number is prime, otherwise it's not
+		if (testPrime % i == 0 ){
+			return 0; // returns 0 when no error
+		}
+	}
 
 }
 
 int main(int argc,char *argv[]){
-  unsigned int test;
+	unsigned int test;
 
-  printf("Enter a number: ");
-  scanf("%d", &test);
-   
+	// input for the number to be tested
+	printf("Enter a number: ");
+	scanf("%d", &test);
 
-  if (isPrime(test))
-    printf("%d is prime.\n", test);	  
-  else
-    printf("%d is not prime.\n", test);
+	// prints out whether the number is prime or n ot
+	if (isPrime(test))
+		printf("%d is prime.\n", test);	  
+	else
+		printf("%d is not prime.\n", test);
 
-  printf("argc is %d\n", argc);
-  for (int i = 0; i < argc; i++){
-    printf("argv[%d] is %s\n", i, argv[i]);
-  }
+	printf("argc is %d\n", argc);
+	for (int i = 0; i < argc; i++){
+		printf("argv[%d] is %s\n", i, argv[i]);
+	}
 
-  return 0;
+	return 0;
 }
 
