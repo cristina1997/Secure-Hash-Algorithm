2ed68cd2851c3103b1934e3657e16f31f604316a
Cristina Nita <cristinaflorinaeu@gmail.com>
2019-03-17 22:02:51 +0000

    Padding with block < 55 bytes

--- Padding/padfile.c
+++ Padding/padfile.c
@@ -23,16 +23,16 @@
 ** that might have different types.
 */
 union msgBlock{
-  uint8_t e[64];  // 8  bits(type) * 64 = 512
-  uint32_t t[16]; // 32 bits(type) * 16 = 512
-  uint64_t s[8];  // 64 bits(type) * 8  = 512
+  uint8_t e[64];                          // 8  bits(type) * 64 = 512
+  uint32_t t[16];                         // 32 bits(type) * 16 = 512
+  uint64_t s[8];                          // 64 bits(type) * 8  = 512
 };
 
 int main(int argc, char *argv[]){ 
-  FILE* fp = fopen(argv[1], "r"); // reads a file
-  union msgBlock M;               // initialization of union message block
-  uint64_t numBytes;              // number of bytes -> number between 0 - 64
-  uint64_t numBits = 0;           // number of bits  -> all bits read
+  FILE* fp = fopen(argv[1], "r");         // reads a file
+  union msgBlock M;                       // initialization of union message block
+  uint64_t numBytes;                      // number of bytes -> number between 0 - 64
+  uint64_t numBits = 0;                   // number of bits  -> all bits read
 
   /* fopen returns 0, the NULL pointer, on failure */
   /* Source code
@@ -46,24 +46,31 @@ int main(int argc, char *argv[]){
 
     while (!feof(fp)) {
       numBytes = fread(M.e, 1, 64, fp);
-      numBits = numBits + (NumBytes * 8);
-      
+      numBits += numBytes * 8;      
+      // printf("Bytes: %2llu\n", numBytes);  // shows the number of bytes with padding
+
       if (numBytes <= 64-9) {
         printf("Block with less than 56 bytes! \n");
-        // M.e[numBytes] = 0x01;  // right most position
-        M.e[numBytes] = 0x80;     // left most position
+        // M.e[numBytes] = 0x01;          // right most position
+        M.e[numBytes] = 0x80;             // left most position
         
         while (numBytes <= 64-9 ){
           numBytes += 1;
-          M.e[numBytes] = 0x00; // add k amount of 0's before appending
+          M.e[numBytes] = 0x00;           // add k amount of 0's before appending
         } // while
-        M.s[7] = numbits;
+        M.s[7] = numBits;
 
       } // if
 
     } // while
 
     fclose(fp);
+
+    for (int i = 0; i < 65; i++) {
+      printf("%x ", M.e[i]);
+    }
+
+    printf("\n");
   } // if.. else
 	return 0;
 }
