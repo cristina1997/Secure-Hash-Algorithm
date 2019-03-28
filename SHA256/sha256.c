6132fae24d0e1121c90e32aa88e036a4aa8e64fa
unknown <cristinaflorinaeu@gmail.com>
2019-03-28 23:15:22 +0000

    Finished adding pointers and comments

--- SHA256/sha256.c
+++ SHA256/sha256.c
@@ -43,10 +43,10 @@ union msgBlock{
 ** - FINISH   = 3
 */
 enum status {
-	READ = 0, 
-	PAD0 = 1, 
-	PAD1 = 2, 
-	FINISH = 3,
+	READ, 
+	PAD0, 
+	PAD1, 
+	FINISH,
 };             
 
 
@@ -74,14 +74,28 @@ uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
 
 int main(int argc, char *argv []){ 
 
-  FILE *fp = fopen(argv [1], "r");		// reads a file
-	
-	sha256(fp);
+	FILE *fp = fopen(argv [1], "r");		// reads a file
+		
+ 	/* If no file to be opened is mentioned
+	** let the user know that no file was mentioned.
+  	** Source code
+    ** - https://stackoverflow.com/questions/9449295/opening-a-fp-from-command-line-arguments-in-c
+    */
+	// if (fp == 0)
+    // {
+    //     printf("No file to be open mentioned.\n");
+    // } else {	
+	// 	sha256(fp);							// run secure hash algorithm
+    // } // if.. else
+
+	sha256(fp);							// run secure hash algorithm
+    fclose(fp);								// close the file
+
 	return 0;
-}    
+} // int main() method
 
 int nextMsgBlock(FILE *fp, union msgBlock *M, enum status *S, int *numBits){ 
-   uint64_t numBytes;						// number of bytes -> between 0 - 64
+	uint64_t numBytes;						// number of bytes -> between 0 - 64
 
 	// All message blocks are finished
 	if (*S == FINISH){
@@ -90,95 +104,63 @@ int nextMsgBlock(FILE *fp, union msgBlock *M, enum status *S, int *numBits){
 	
 	/* If a block full of padding is needed 
 	** then set first 56 bytes to 0 bits
-	** set the last 64 bits to the no. of bits in the file
-	** and tell S we are finished.
+	** set the last 64 bits to the no. of bits in the file (big endian)
+	** Status == finished.
 	** If S was PAD1 then set the first bit of M to 1.
 	** - PAD0 - first bit is 0
 	** - PAD1 - first bit is 1
 	*/
 	if (*S == PAD0 || *S == PAD1) {
-		for (int i = 0; i <= 64-9; i++)			// leaves 8 bytes at the end for the 64 bit integer
+		for (int i = 0; i <= 64-9; i++)		// leaves 8 bytes at the end for the 64 bit integer
 			M -> e [i] = 0x00;				// padding the 448 bits with 0s
+
 
 		M -> s [7] = *numBits;       
 		*S = FINISH;
 
 		
-		if (*S == PAD1) {
+		if (*S == PAD1)
 			M -> e [0] = 0x80;  
 
-		return 1;           
+		return 1;   
 	} // if
 
 
-	} // if
+   	// The file hasn't finished reading (S is equal to READ)    
+	numBytes = fread(M -> e, 1, 64, fp);
+	*numBits += numBytes * 8;      
 
+	// If less than 56 bytes is read then add padding
+	if (numBytes <= 64-9) {
+		printf("Block with less than 55 bytes! \n");
+		// M.e [numBytes] = 0x01;			// first byte in M that hasn't been overriden
+											// -> right most position
+		M -> e [numBytes] = 0x80;			// first byte in M that hasn't been overriden
+											// -> left most position
+		
+		while (numBytes <= 64-9){
+			numBytes += 1;
+			M -> e [numBytes] = 0x00;		// add k amount of 0s before appending
+		} // while
 
-    /* If no file to be opened is mentioned
-	  ** let the user know that no file was mentioned.
-  	** Source code
-    ** - https://stackoverflow.com/questions/9449295/opening-a-fp-from-command-line-arguments-in-c
-    */
-/*    if (fp == NULL)
-    {
-        printf("No file to be open mentioned.\n");
-    } else { */     
+		M -> s [7] = *numBits;				// append file size in bits (big endian) - 64 bit int
+		*S = FINISH;						// Status == finished
 
-        while (*S == READ) {
-            numBytes = fread(M -> e, 1, 64, fp);
-            numBits += numBytes * 8;      
-            // printf("Bytes: %2llu\n", numBytes);  // shows the number of bytes with padding
+	} else if (numBytes < 64) {				// check if there's enough space for paddings
+		*S = PAD0;							// it aknowledges a message block containing all 0s is needed
+		M -> e [numBytes] = 0x80;			// insert 1 bit in current block
 
-            if (numBytes <= 64-9) {
-                printf("Block with less than 55 bytes! \n");
-                // M.e [numBytes] = 0x01;            // first byte in M that hasn't been overriden
-                                                    // -> right most position
-                M -> e [numBytes] = 0x80;               // first byte in M that hasn't been overriden
-                                                    // -> left most position
+		while (numBytes < 64) {				
+			numBytes += 1;
+			M -> e [numBytes] = 0x00;		// padd the rest with 0s
+		} // while
+	} else if (feof(fp)) { 					// check if we're at the end of the file
+		*S = PAD1;							// another message block with padding is needed
+	} // if.. else if
 
-                while (numBytes <= 64-9){
-                    numBytes += 1;
-                    M -> e [numBytes] = 0x00;             // add k amount of 0s before appending
-                } // while
-                M -> s [7] = *numBits;
-                *S = FINISH;
-
-            } else if (numBytes < 64) {
-                *S = PAD0;                           // it aknowledges a message block containing all 0s is needed
-                M -> e [numBytes] = 0x80;
-
-                while (numBytes < 64) {
-                    numBytes += 1;
-                    M -> e [numBytes] = 0x00;           // padd with 0s
-                } // while
-            } else if (feof(fp)) {
-                *S = PAD1;
-            } // if.. else if
-
-        } // while
-
-        /* PAD0 - first bit is 0
-        ** PAD1 - first bit is 1
-        */
-        if (*S == PAD0 || *S == PAD1) {
-            for (int i = 0; i <= 64-9; i++)              // leaves 8 bytes at the end for the 64 bit integer
-                M -> e [i] = 0x00;                      // padding the 448 bits with 0s
-            M -> s [7] = *numBits;                       
-        } // if
-
-        if (*S == PAD1) {
-            M -> e [0] = 0x80;
-        } // if
-        fclose(fp);
-
-        for (int i = 0; i <= 64; i++) {
-            printf("%x ", M -> e [i]);
-        } // for loop
-
-        printf("\n");
-    // } // if.. else
-    return 0;
-}
+	return 1;								// the function is called again
+	
+} // int nextMsgBlock() method
 
 void sha256(fp){
 	// SHA Calculation variables
@@ -188,40 +170,34 @@ void sha256(fp){
 	
 	// Padding Calculation variables
 	union msgBlock M;						// current message block
-  enum status S = READ;					// message blocks status
-  uint64_t numBits = 0;					// number of bits read
+  	enum status S = READ;					// message blocks status
+  	uint64_t numBits = 0;					// number of bits read
 
 	// Hash Value - Section 5.3.3
 	uint32_t H [8] = {
-		0x6a09e667, 
-		0xbb67ae85, 
-		0x3c6ef372, 
-		0xa54ff53a, 
-		0x510e527f, 
-		0x9b05688c, 
-		0x1f83d9ab, 
-		0x5be0cd19		
-	};
+		0x6a09e667, 	0xbb67ae85, 	0x3c6ef372, 	0xa54ff53a, 
+		0x510e527f, 	0x9b05688c, 	0x1f83d9ab, 	0x5be0cd19		
+	}; // H[8]
 
 	// Constant for the hash computation - Section 4.2.2
 	uint32_t K [] = {
-		0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
-		0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 
-		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 
-		0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 
-		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 
-		0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 
-		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 
-		0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 
-		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 
-		0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 
-		0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 
-		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 
-		0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 
-		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 
-		0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-	};
+		0x428a2f98, 	0x71374491, 	0xb5c0fbcf, 	0xe9b5dba5, 
+		0x3956c25b, 	0x59f111f1, 	0x923f82a4, 	0xab1c5ed5, 
+		0xd807aa98, 	0x12835b01, 	0x243185be, 	0x550c7dc3, 
+		0x72be5d74, 	0x80deb1fe, 	0x9bdc06a7, 	0xc19bf174, 
+		0xe49b69c1, 	0xefbe4786, 	0x0fc19dc6, 	0x240ca1cc, 
+		0x2de92c6f, 	0x4a7484aa, 	0x5cb0a9dc, 	0x76f988da, 
+		0x983e5152, 	0xa831c66d, 	0xb00327c8, 	0xbf597fc7, 
+		0xc6e00bf3, 	0xd5a79147, 	0x06ca6351, 	0x14292967, 
+		0x27b70a85, 	0x2e1b2138, 	0x4d2c6dfc, 	0x53380d13, 
+		0x650a7354, 	0x766a0abb, 	0x81c2c92e, 	0x92722c85,
+		0xa2bfe8a1, 	0xa81a664b, 	0xc24b8b70, 	0xc76c51a3, 
+		0xd192e819, 	0xd6990624, 	0xf40e3585, 	0x106aa070, 
+		0x19a4c116, 	0x1e376c08, 	0x2748774c, 	0x34b0bcb5, 
+		0x391c0cb3, 	0x4ed8aa4a, 	0x5b9cca4f, 	0x682e6ff3, 
+		0x748f82ee, 	0x78a5636f, 	0x84c87814, 	0x8cc70208, 
+		0x90befffa, 	0xa4506ceb, 	0xbef9a3f7, 	0xc67178f2
+	}; // K[]
 
 	// Loop through message blocks as per page 22
 	while (nextMsgBlock(fp, M, S, numBits)){
@@ -234,7 +210,7 @@ void sha256(fp){
 		// Loops through the next 48 elements of W [] - Step 1 Page 24
 		for (int i = 16; i < 64; i++)
 			W  [i] = sig1(W [i-2]) + W [i-7] + sig0(W [i-15]) + W [i-16];
-		
+				
 		// Initialize a, b, c, d, e - Step 2 Page 22
 		a = H [0]; b = H [1]; c = H [2]; d = H [3]; 
 		e = H [4]; f = H [5]; g = H [6]; h = H [7];
@@ -247,25 +223,18 @@ void sha256(fp){
 			e = d + T1;
 			d = c; c = b; b = a;
 			a = T1 + T2;
-		}	
+		} // for
 		
 		// Step 4 - Page 23
-		H [0] = a + H [0];
-		H [1] = b + H [1];
-		H [2] = c + H [2];	
-		H [3] = d + H [3];
-		H [4] = e + H [4];
-		H [5] = f + H [5];
-		H [6] = g + H [6];
-		H [7] = h + H [7];
-	}
+		H [0] = a + H [0]; 	H [1] = b + H [1];	H [2] = c + H [2];	H [3] = d + H [3];
+		H [4] = e + H [4];	H [5] = f + H [5];	H [6] = g + H [6];	H [7] = h + H [7];
+	} // while
 	
 
-i  for (int i = 0; i < 8; i++) {
-    printf("%x \n", H [i]);
-  }
+	for (int i = 0; i < 8; i++)
+		printf("%x \n", H [i]);	
 	
-}
+} // void sha256() method
 
 /************* ROTR_n(x) **************/
 /*** shift n positions to the right ***/
