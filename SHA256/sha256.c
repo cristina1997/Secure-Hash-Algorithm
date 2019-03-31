28dd2c9da24268ef93d418641c08650fb15ad71b
unknown <cristinaflorinaeu@gmail.com>
2019-03-31 15:19:14 +0100

    Added header for message status

--- SHA256/sha256.c
+++ SHA256/sha256.c
@@ -38,20 +38,7 @@
 #include "headers/rotate-def.h"
 #include "headers/sha256-methods.h"
 #include "headers/message-block.h"
-
-/* Flag for File Read
-** - READ     = 0
-** - PAD0     = 1
-** - PAD1     = 2
-** - FINISH   = 3
-*/
-enum status {
-	READ, 
-	PAD0, 
-	PAD1, 
-	FINISH,
-};             
-
+#include "headers/status.h"
 
 /** 
 ***	Declaration of methods
