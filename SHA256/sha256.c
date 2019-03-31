fc6de980485525909a7c09cfeaee5474abdf7589
unknown <cristinaflorinaeu@gmail.com>
2019-03-31 15:17:35 +0100

    Added header for message block

--- SHA256/sha256.c
+++ SHA256/sha256.c
@@ -37,22 +37,7 @@
 #include "headers/big-endians.h"
 #include "headers/rotate-def.h"
 #include "headers/sha256-methods.h"
-
-
-/* Union
-** - represents a message block
-** - used to read file in blocks of 512 bits
-** - like structures (structs)
-** - can only store one value at a time
-** - stored in the same location.
-** - used in variables related to each other
-**   that might have different types.
-*/
-union msgBlock{
-    uint8_t  e [64];                                   // 8  bits(type) * 64 = 512
-    uint32_t t [16];                                   // 32 bits(type) * 16 = 512
-    uint64_t s [8];                                    // 64 bits(type) * 8  = 512
-};
+#include "headers/message-block.h"
 
 /* Flag for File Read
 ** - READ     = 0
